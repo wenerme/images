19f3df21f46edfe7b9b9ac357409ed506c8b813e
wener <wenermail@gmail.com>
2020-11-17 15:23:04 +0800

    manual update

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -19,7 +19,7 @@ sync-image k8s.gcr.io/defaultbackend-amd64 1.5
 # quay.io/kubernetes-ingress-controller/nginx-ingress-controller
 # sync-image quay.io/kubernetes-ingress-controller/nginx-ingress-controller 0.33.0
 # sync-image us.gcr.io/k8s-artifacts-prod/ingress-nginx/controller v0.34.1
-sync-image k8s.gcr.io/ingress-nginx/controller v0.35.0
+sync-image k8s.gcr.io/ingress-nginx/controller v0.41.2
 
 sync-image gcr.io/google_containers/defaultbackend 1.0
 
@@ -33,7 +33,7 @@ sync-image quay.io/jetstack/cert-manager-acmesolver $ver
 ver=$(github-latest-version thanos-io/thanos)
 sync-image quay.io/thanos/thanos $ver
 
-sync-image docker.io/jettech/kube-webhook-certgen v1.2.2
+sync-image docker.io/jettech/kube-webhook-certgen v1.5.0
 
 for i in proxy controller debug web grafana cni-plugin; do
   sync-image gcr.io/linkerd-io/$i stable-2.8.1
