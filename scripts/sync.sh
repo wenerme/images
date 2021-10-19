cf2687037cef94c9288696f64eb208042da0d114
陈杨文 <wenermail@gmail.com>
2021-10-19 20:47:14 +0800

    acid/spilo-14

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -23,7 +23,7 @@ sync-image registry.opensource.zalan.do/acid/postgres-operator $(github-latest-v
 sync-image registry.opensource.zalan.do/acid/logical-backup $(github-latest-version zalando/postgres-operator)
 sync-image registry.opensource.zalan.do/acid/postgres-operator-ui $(github-latest-version zalando/postgres-operator)
 
-sync-image registry.opensource.zalan.do/acid/spilo-13 $(github-latest-version zalando/spilo)
+sync-image registry.opensource.zalan.do/acid/spilo-14 $(github-latest-version zalando/spilo)
 sync-image registry.opensource.zalan.do/acid/pgbouncer master-16
 
 # nfs
