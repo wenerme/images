bf70fe45a8462e5b008e0d33e848c349daca38c0
wener <wenermail@gmail.com>
2022-03-06 18:20:47 +0800

    disable ingress-nginx/controller

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -60,8 +60,8 @@ sync-image quay.io/thanos/thanos $ver
 # sync-image gcr.io/google-containers/pause 3.2
 # sync-image k8s.gcr.io/defaultbackend-amd64 1.5
 
-sync-image k8s.gcr.io/ingress-nginx/controller v$(chart-app-version ingress-nginx)
-sync-image k8s.gcr.io/ingress-nginx/kube-webhook-certgen v1.1.1
+# sync-image k8s.gcr.io/ingress-nginx/controller v$(chart-app-version ingress-nginx)
+# sync-image k8s.gcr.io/ingress-nginx/kube-webhook-certgen v1.1.1
 
 # sync-image gcr.io/google_containers/defaultbackend 1.0
 
